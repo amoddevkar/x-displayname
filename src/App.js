4be9c0ff8c70d4845a51e319ec8627e1010b57d8
amoddevkar <87513080+amoddevkar@users.noreply.github.com>
2024-09-21 20:23:08 +0530

    initial commit1

--- src/App.js
+++ src/App.js
@@ -7,6 +7,22 @@ function App() {
   const [lName, setLName] = useState('')
   const [display, setDisplay] = useState(false)
 
+  const handleFname = (e) => {
+    var term = e.target.value;
+    var re = new RegExp("[a-zA-Z]");
+    if (re.test(term)) {
+      setFName(term)
+    }
+
+  }
+  const handleLname = (e) => {
+    var term = e.target.value;
+    var re = new RegExp("[a-zA-Z]");
+    if (re.test(term)) {
+      setLName(term)
+    }
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault()
     setDisplay(true)
@@ -15,10 +31,11 @@ function App() {
     <div className="App">
       <h1>Full Name Display</h1>
       <form onSubmit={handleSubmit}>
-        <label for='firstname'>First Name:</label><input pattern="[a-zA-Z]*" type='text' name='firstname' onChange={(e) => { setFName(e.target.value) }} required /><br />
-        <label for='lastanme'>Last Name:</label><input pattern="[a-zA-Z]*" type='text' name='lastname' onChange={(e) => { setLName(e.target.value) }} required /><br />
+        <label for='firstname'>First Name:</label><input pattern="[a-zA-Z]*" type='text' name='firstname' onChange={handleFname} required /><br />
+        <label for='lastanme'>Last Name:</label><input pattern="[a-zA-Z]*" type='text' name='lastname' onChange={handleLname} required /><br />
         <button type='submit'>Submit</button>
       </form>
+
       {display ? <p>Full Name: {`${fName} ${lName}`}</p> : <p></p>}
     </div>
   );
